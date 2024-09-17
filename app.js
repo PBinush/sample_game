801bc7dd461cd3f37f6a9f499a92b01c14319d90
PBinush <pasidbinush@gmail.com>
2024-09-17 14:15:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,168 @@
+// class Customer{
+//     name;
+//     age;
+//     address;
+
+
+// Customer(name,age,address) {
+//     this.name=name;
+//     this.age=age;
+//     this.address=address;
+// }
+
+// setName(name){
+// this.name=name;
+// }
+
+// getName(){
+//     return this.name;
+// }
+// }
+// let customer01 = new Customer("saman",23,"galle");
+
+// console.log(customer01.getName());
+
+let randomNumber = Math.floor(Math.random()*10+1);
+console.log(randomNumber);
+let lblOutput = document.getElementById("lbl");
+
+function btn1(){
+    if(randomNumber==1){
+         lblOutput.innerHTML = `you win!🥳`;
+         lblOutput.style.color = `green`;
+         alert(`you win!🥳🥳`);
+         
+    }else if(randomNumber<1){
+        lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+        lblOutput.style.color=`red`;
+        alert(`you win!🥳🥳`);
+    }else{
+        lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+        lblOutput.style.color=`red`;
+    }  
+}
+function btn2(){
+    if(randomNumber==2){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<2){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   }  
+}
+function btn3(){
+    if(randomNumber==3){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<3){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   }  
+    
+}
+function btn4(){
+    if(randomNumber==4){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<4){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   }
+    
+}
+function btn5(){
+    if(randomNumber==5){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<5){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   }
+    
+}
+function btn6(){
+    if(randomNumber==6){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<6){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   }
+    
+}
+function btn7(){
+    if(randomNumber==7){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<7){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   }
+    
+}
+function btn8(){
+    if(randomNumber==8){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<8){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   };
+    
+}
+function btn9(){
+    if(randomNumber==9){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<9){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   }
+
+}
+function btn10(){
+    if(randomNumber==10){
+        lblOutput.innerHTML = `you win!🥳`;
+        lblOutput.style.color = `green`;
+        alert(`you win!🥳🥳`);
+   }else if(randomNumber<10){
+       lblOutput.innerHTML= `Too high!🥵 Try and lower number...😝`
+       lblOutput.style.color=`red`;
+   }else{
+       lblOutput.innerHTML= `Too low!🥶 Try and higher number...😝`
+       lblOutput.style.color=`red`;
+   }
+
+}
